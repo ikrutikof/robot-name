9c1ddd2fdb9310ebfcdb6bfb99cce6f66220d863
Ivan <60300149+ikrutikof@users.noreply.github.com>
2020-11-01 16:20:19 +0300

    upd

--- robot-name.js
+++ robot-name.js
@@ -7,29 +7,29 @@ export class Robot {
     static usedNames = new Set();
 
     constructor () {
-        
-        this.setName = () => {
-            let name
-            do {
-                name = this.generateName();
-            } while (Robot.usedNames.has (name) )
-            Robot.usedNames.add(name);
-            return name 
-        };
-        this.generateName = () => {
-            const   letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-                    digit = '0123456789' ,
-                    random = (string) => { return string[ Math.floor ( Math.random() * (string.length ) ) ] }
-            let name = random(letter) + random(letter) + random(digit) + random(digit) + random(digit);
-    
-            return name
-        };
-        this._name = setName();
-
+        this._name = this.setName();
     }
 
     get name() {
         return this._name
+    }
+
+    setName = () => {
+        let name
+        do {
+            name = this.generateName();
+        } while (Robot.usedNames.has (name) )
+        Robot.usedNames.add(name);
+        return name
+    }
+
+    generateName = () => {
+        const   letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                digit = '0123456789' ,
+                random = (string) => { return string[ Math.floor ( Math.random() * (string.length ) ) ] }
+        let name = random(letter) + random(letter) + random(digit) + random(digit) + random(digit);
+
+        return name
     }
 
     reset = () => {
